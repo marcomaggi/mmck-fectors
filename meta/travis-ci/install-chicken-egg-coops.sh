acee9e4a9eb950288a5288222d70ec9678060597
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-04-24 08:29:29 +0200

    added chicken egg coops installation to travis infrastructure

---
+++ meta/travis-ci/install-chicken-egg-coops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Installation  script to  run from  the  Travis CI  config file  before
+# attempting a build.
+#
+# Install the CHICKEN egg "coops"  under the directory "/usr/local".  We
+# assume the script is run from the top directory of the build tree.
+
+PROGNAME=${0##*/}
+
+prefix=/usr/local
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
+
+if test "$TRAVIS_OS_NAME" = osx
+then THE_PLATFORM=macosx
+else THE_PLATFORM=linux
+fi
+
+function main () {
+    chicken-install coops
+}
+
+function script_error () {
+    local TEMPLATE="${1:?missing template argument to '$FUNCNAME'}"
+    shift
+    {
+	printf '%s: ' "$PROGNAME"
+	printf "$TEMPLATE" "$@"
+	printf '\n'
+    } >&2
+    exit 1
+}
+
+main "$@"
+
+### end of file
